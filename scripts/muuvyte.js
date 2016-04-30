124b40507b3fb04c8f348072be86bb1761f8030c
Jeff Brannon <Jeff Brannon>
2016-04-30 18:52:02 +0100

    Base Linked Object

--- scripts/muuvyte.js
+++ scripts/muuvyte.js
@@ -2,9 +2,29 @@
 
     var modules = [];
 
-    var moduleInstance = {
-        linkedObject: function(){ console.log(this.$$moduleName, 'Hello'); }
-    };
+    var linkedObjectInstance = { };
+
+    var linkedObject = function(name, linkedObj, params = {}) {
+        if(linkedObj) {
+            if(this.$$linkedObjects[name] !== undefined) muuvyte.throwError('linked object <' + name + '> already defined');
+            return this.$$linkedObjects[name] = Object.create(linkedObjectInstance, {
+                $$linkedObjectName: { value: name },
+                $$linkedObject: { value: linkedObj },
+                $$params: { value: params }
+            });
+        }
+
+        if(!this.$$linkedModules) {
+            this.$$linkedModules = linkedModules(this.$$moduleName);
+        }
+
+        var result;
+        for(var propt in this.$$linkedModules) {
+            result = this.$$linkedModules[propt].$$linkedObjects[name];
+            if(result) return result;
+        }
+        muuvyte.throwError('linked object <' + name + '> undefined');
+    }
 
     var linkedModules = function(name, list = []) {
         list.push(modules[name]);
@@ -15,31 +35,37 @@
         return list;
     }
 
+    var baseLinkedObject = function(baseObject) {
+        this.$$baseObject = Object.assign(this.$$baseObject || {}, baseObject);
+    };
+
+    var moduleInstance = {
+        linkedObject: linkedObject,
+        baseLinkedObject: baseLinkedObject
+    };
+
     var module = function(name, requires) {
         if(requires) {
             if(!Array.isArray(requires)) muuvyte.throwError('<requires> must be of type array');
+            if(modules[name] !== undefined) muuvyte.throwError('module <' + name + '> already defined');
             modules[name] = Object.create(moduleInstance, {
                 $$moduleName: { value: name },
-                $$moduleRequires: { value: requires }
+                $$moduleRequires: { value: requires },
+                $$linkedObjects: { value: [] }
             });
         }
+        if(modules[name] === undefined) muuvyte.throwError('module <' + name + '> undefined');
         return modules[name];
     };
 
-    function throwError(message) {
+    var throwError = function(message) {
         throw message;
     }
 
     var muuvyte = {
         module: module,
-        throwError: throwError,
-        linkedModules: function(name) {
-            var result = linkedModules(name);
-            for(var propt in result) {
-                console.log('zz', result[propt].$$moduleName, result[propt]);
-                result[propt].linkedObject();
-            }
-        }
+        baseLinkedObject: baseLinkedObject,
+        throwError: throwError
     };
 
     window['muuvyte'] = muuvyte;
@@ -51,93 +77,20 @@ muuvyte.module('module2', []);
 muuvyte.module('module3', ['module4']);
 muuvyte.module('module4', []);
 
-muuvyte.module('module1');
-var f = muuvyte.module('module1');
-console.log(f);
-f.linkedObject();
+
+var module1 = muuvyte.module('module1');
+module1.linkedObject('object1', { name: 'Jeff' });
+
+var module3 = muuvyte.module('module3');
+module3.linkedObject('object2', { name: 'Susie' });
 
 
-var d = muuvyte.linkedModules('module3');
+console.log(module1.linkedObject('object1'));
+console.log(muuvyte.module('module3').linkedObject('object2'));
 
-//muuvyte.module('module2', []);
-//muuvyte.module('module3', ['module2']);
-//muuvyte.module('module1', ['module3']);
-//
-//muuvyte.module('module2').linkedObject('jeff', {forename:'Jeff'});
-//muuvyte.module('module3').linkedObject('susie', {forename:'Susie'});
-//
-//
-//var d = muuvyte.module('module1').linkedObject('jeff');
-//console.log(d);
-//
-//
-//var f1 = muuvyte.module('module1');
-//console.log(1, f1);
-//var f2 = muuvyte.module('module2');
-//console.log(2, f2);
-//var f3 = muuvyte.module('module3');
-//console.log(3, f3);
-
-
-//var s = [];
-//s['jeff'] = { forename:'Jeff' };
-//s['susie'] = { forename:'Susie' };
-//console.log(s);
-
-//for(var propt in s){
-//    console.log(propt, s[propt]);
-//}
-
-
-//muuvyte.linkWithBaseObject({name: 'jeff'});
-//var g = muuvyte.linkWithBaseObject({age: 44});
-//console.log(g);
-
-//console.log(muuvyte.baseObjectComponentManager.components[''])
-
-
-
-
-
-
-//Object.defineProperty(uj, "a", {
-//    value: 2,
-//    writable: true,
-//    configurable: true,
-//    enumerable: true
-//});
-
-
-
-//var Bar = Object.create(uj);
-//Bar.constructor();
-
-//Bar.module()();
-//console.log(Bar.a);
-
-
-
-//var m = uj.module('module1',{
-//    reference: ['module2', 'module3']
-//});
-//
-//m.class('service1', {
-//    singleton: true,
-//    constructor: function() {}
-//});
-//
-//m.class('class1', {
-//    internal: true,
-//    mustInherit: true,
-//    constructor: function() {}
-//});
-//
-//m.class('class2', {
-//    extends: 'class1',
-//    constructor: ['service1', function(service1) {}]
-//});
-//
-//m.enum('enum1', {
-//    item1: 0,
-//    item2: 1
-//});
+muuvyte.baseLinkedObject({item1: 'item1'});
+console.log(muuvyte);
+muuvyte.module('module1').baseLinkedObject({item2: 'item2'});
+console.log(muuvyte.module('module1'));
+//muuvyte.baseLinkedObject({item3: 'item3'});
+//console.log(muuvyte);
