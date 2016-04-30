02092c430be12a924332e54482fbeb4aa24ec196
Jeff Brannon <Jeff Brannon>
2016-04-30 19:44:19 +0100

    internal

--- scripts/muuvyte.js
+++ scripts/muuvyte.js
@@ -4,26 +4,53 @@
 
     var linkedObjectInstance = { };
 
+    var findLinkedObject = function(name, self, includeToBeLinked) {
+        if(!self.$$linkedModules) {
+            self.$$linkedModules = linkedModules(self.$$moduleName);
+        }
+        for(var propt in self.$$linkedModules) {
+            result = self.$$linkedModules[propt].$$linkedObjects[name];
+            var result = self.$$linkedModules[propt].$$linkedObjects[name];
+            if(result) {
+                if(result.$$params.internal && self.$$moduleName != result.$$moduleName) muuvyte.throwError('linked object <' + name + '> defined as internal');
+                if(!includeToBeLinked && result.$$params.toBeLinked) muuvyte.throwError('linked object <' + name + '> must be linked');
+
+                return result;
+            }
+        }
+        muuvyte.throwError('linked object <' + name + '> undefined');
+    };
+
     var linkedObject = function(name, linkedObj, params = {}) {
         if(linkedObj) {
             if(this.$$linkedObjects[name] !== undefined) muuvyte.throwError('linked object <' + name + '> already defined');
-            return this.$$linkedObjects[name] = Object.create(linkedObjectInstance, {
-                $$linkedObjectName: { value: name },
-                $$linkedObject: { value: linkedObj },
-                $$params: { value: params }
-            });
+//            return this.$$linkedObjects[name] = Object.create(linkedObjectInstance, {
+//                $$linkedObjectName: { value: name },
+//                $$linkedObject: { value: linkedObj },
+//                $$params: { value: params }
+//            });
+
+            this.$$linkedObjects[name] = {
+                $$linkedObjectName: name,
+                $$linkedObject: linkedObj,
+                $$params: params,
+                $$moduleName: this.$$moduleName
+            };
         }
 
-        if(!this.$$linkedModules) {
-            this.$$linkedModules = linkedModules(this.$$moduleName);
-        }
+        return findLinkedObject(name, this);
 
-        var result;
-        for(var propt in this.$$linkedModules) {
-            result = this.$$linkedModules[propt].$$linkedObjects[name];
-            if(result) return result;
-        }
-        muuvyte.throwError('linked object <' + name + '> undefined');
+//        for(var propt in this.$$linkedModules) {
+//            result = this.$$linkedModules[propt].$$linkedObjects[name];
+//            var result = this.$$linkedModules[propt].$$linkedObjects[name];
+//            if(result) {
+//                if(result.$$params.internal && this.$$moduleName != result.$$moduleName) muuvyte.throwError('linked object <' + name + '> defined as internal');
+//                if(result.$$params.toBeLinked) muuvyte.throwError('linked object <' + name + '> must be linked');
+//
+//                return result;
+//            }
+//        }
+//        muuvyte.throwError('linked object <' + name + '> undefined');
     }
 
     var linkedModules = function(name, list = []) {
@@ -79,18 +106,17 @@ muuvyte.module('module4', []);
 
 
 var module1 = muuvyte.module('module1');
-module1.linkedObject('object1', { name: 'Jeff' });
+module1.linkedObject('object1', { name: 'Jeff' }, { internal: true });
 
 var module3 = muuvyte.module('module3');
-module3.linkedObject('object2', { name: 'Susie' });
+module3.linkedObject('object2', { name: 'Susie' }, {});
 
 
 console.log(module1.linkedObject('object1'));
-console.log(muuvyte.module('module3').linkedObject('object2'));
+console.log(muuvyte.module('module1').linkedObject('object2'));
 
-muuvyte.baseLinkedObject({item1: 'item1'});
-console.log(muuvyte);
-muuvyte.module('module1').baseLinkedObject({item2: 'item2'});
-console.log(muuvyte.module('module1'));
-//muuvyte.baseLinkedObject({item3: 'item3'});
+//muuvyte.baseLinkedObject({item1: 'item1'});
 //console.log(muuvyte);
+//muuvyte.module('module1').baseLinkedObject({item2: 'item2'});
+//console.log(muuvyte.module('module1'));
+
