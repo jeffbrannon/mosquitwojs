d0d1e061aba7dd880e86c70a21f90e91f89a3231
Jeff Brannon <Jeff.Brannon@cm2000.co.uk>
2016-05-14 09:28:49 +0100

    inject

--- scripts/injector.js
+++ scripts/injector.js
@@ -1,2 +1,16 @@
 'use strict';
 
+(function(){
+
+    muuvyte.$inject = function(injection) {
+        return function() {
+            this.$inject(injection);
+        }
+    }
+
+    muuvyte.baseLinkedObject({
+        $inject: function(injection) {
+            console.log(this, injection);
+        }
+    });
+})();
